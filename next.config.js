fd6373151827b0230bcc39c71f55fd24ad737060
h-jolly <jprrooe@gmail.com>
2022-10-07 12:16:35 +0900

    feat: 개발 환경 초기 구성(Next.js, prettier, eslint, storybook)
     - 개발 환경 초기 구성(Next.js, prettier, eslint, storybook)

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  emotion: true,
+  styledComponents: true,
+};
+
+module.exports = nextConfig;
